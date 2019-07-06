409d03450aa43036f0812da381deb56c28fda398
Adam <adam.lj.haglund@gmail.com>
2019-07-06 23:01:32 +0200

    enable introspection

--- src/apollo.ts
+++ src/apollo.ts
@@ -5,6 +5,7 @@ import { createSchema } from '@/graphql'
 export const createServer = async () => {
   const server = new ApolloServer({
     schema: await createSchema(),
+    introspection: true,
   })
 
   return server
