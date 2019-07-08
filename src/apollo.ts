c5521293058bc560f314ccf2348ac658a161a48e
Adam <adam.lj.haglund@gmail.com>
2019-07-08 20:14:36 +0200

    add helmet

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,11 +1,14 @@
 import { ApolloServer } from 'apollo-server-express'
 import Express, { Express as IExpress } from 'express'
+import Helmet from 'helmet'
 
 import { createSchema } from '@/graphql'
 import { router } from '@/router'
 
 export const createApp = (): IExpress => {
   const app = Express()
+
+  app.use(Helmet())
 
   app.use(router)
 
