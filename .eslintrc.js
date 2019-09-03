05755968afbcab4313d0367e0247a950d3d13c80
Adam Haglund <adam.lj.haglund@gmail.com>
2019-09-03 23:16:13 +0200

    Frontend (#3)
    * add base
    
    * add ui

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true,
     jest: true,
   },
   parser: '@typescript-eslint/parser',
---
+++ packages/ui/src/app.tsx
@@ -0,0 +1,32 @@
+// Hot loader must be imported first
+import { hot } from 'react-hot-loader/root'
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
+import loadable from '@loadable/component'
+
+import { client } from '@/apollo'
+import { Page } from '@/pages/constants'
+
+const LandingPage = loadable(() =>
+  import(/* webpackChunkName: "landing" */ './pages/landing/landing'),
+)
+
+const AppComponent = () => (
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path={Page.LANDING}
+            key={Page.LANDING}
+            component={LandingPage}
+          />
+        </Switch>
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode>
+)
+
+export const App = hot(AppComponent)
