b6f087f6d581789e45cba3a822c52c1e542d1e09
Adam <adam.haglund@izettle.com>
2019-09-05 15:43:22 +0200

    add match resolver

--- packages/server/src/graphql/index.ts
+++ packages/server/src/graphql/index.ts
@@ -2,10 +2,11 @@ import { buildSchema } from 'type-graphql'
 
 import { BoardgameResolver } from '@/modules/boardgame/boardgame.resolvers'
 import { ClubResolver } from '@/modules/club/club.resolvers'
+import { MatchResolver } from '@/modules/match/match.resolvers'
 import { UserResolver } from '@/modules/user/user.resolvers'
 
 export const createSchema = async () =>
   buildSchema({
     dateScalarMode: 'isoDate',
-    resolvers: [BoardgameResolver, ClubResolver, UserResolver],
+    resolvers: [BoardgameResolver, ClubResolver, MatchResolver, UserResolver],
   })
