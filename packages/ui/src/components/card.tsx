35b521765942ef7b7eca076df26ad4be6ed95976
Angel Paredes Barato <angel.barato@izettle.com>
2019-11-08 09:58:52 +0100

    Add todo

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -1,6 +1,6 @@
 import styled, { keyframes } from 'styled-components'
 
-
+// TODO: Extract palette and common design
 export const GenericInfo = styled.div`
   display: flex;
   justify-content: space-between;
@@ -29,7 +29,7 @@ export const GameName = styled.h3`
   font-weight: 300;
   font-size: 12px;
   line-height: 16px;
-  margin:0;
+  margin: 0;
 `
 export const Title = styled.h2`
   font-size: 24px;
@@ -43,7 +43,7 @@ export const StatBox = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
-  
+
   text-align: center;
   padding: 8px 2px 0;
   &:not(:last-child) {
@@ -64,17 +64,16 @@ const Gradient = keyframes`
   }
 `
 
-
 // Palette https://colorhunt.co/palette/132247
 export const Card = styled.div`
   position: relative;
   flex-direction: column;
   padding: 16px 16px 32px;
-  
+
   border-radius: 0.5rem 1rem;
   border: solid 1px #278ea5;
   border-bottom: solid 4px white;
-  
+
   background: linear-gradient(45deg, #3bdba3, #5a8fdf, #3bdba3, #5a8fdf);
   background-size: 400% 400%;
   animation: ${Gradient} 10s ease infinite;
