ac7c31c09f16dc73e08a4f22f552c5c209a2165e
Adam <adam.lj.haglund@gmail.com>
2019-11-23 23:08:51 +0100

    add cli argument to only generate snapshot

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,19 +2,31 @@ import 'reflect-metadata'
 
 import { connectToDatabase } from '@/db'
 import { createApp, connectApolloServer } from '@/apollo'
+import { createSchema } from '@/graphql'
 
-const port = process.env.PORT || 3000
+const shouldGenerateSnapshot = process.argv.find(
+  str => str.includes('--snapshot') || str.includes('-shot'),
+)
 
-const start = async () => {
-  await connectToDatabase()
-
-  const app = createApp()
-  await connectApolloServer(app)
-
-  app.listen(port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Listening on ${port}`)
+if (shouldGenerateSnapshot) {
+  createSchema().then(() => {
+    // eslint-disable-next-line no-process-exit
+    process.exit(0)
   })
-}
+} else {
+  const port = process.env.PORT || 3000
 
-start()
+  const start = async () => {
+    await connectToDatabase()
+
+    const app = createApp()
+    await connectApolloServer(app)
+
+    app.listen(port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Listening on ${port}`)
+    })
+  }
+
+  start()
+}
