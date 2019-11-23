2c5065a4e7fee01daee72a7bb86f9fe6dae0d36c
Adam <adam.lj.haglund@gmail.com>
2019-11-23 23:07:47 +0100

    generate schema snapshot every server start

--- packages/server/src/graphql/index.ts
+++ packages/server/src/graphql/index.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { buildSchema } from 'type-graphql'
 
 import { BoardgameResolver } from '@/modules/boardgame/boardgame.resolvers'
@@ -7,6 +8,10 @@ import { UserResolver } from '@/modules/user/user.resolvers'
 
 export const createSchema = async () =>
   buildSchema({
+    emitSchemaFile: {
+      commentDescriptions: true,
+      path: resolve(__dirname, 'schema.snapshot'),
+    },
     dateScalarMode: 'isoDate',
     resolvers: [BoardgameResolver, ClubResolver, MatchResolver, UserResolver],
   })
