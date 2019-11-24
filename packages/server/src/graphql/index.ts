754997c4eff8adc4a850372f8be78443787a5f92
Adam Haglund <adam.lj.haglund@gmail.com>
2019-11-24 16:31:47 +0100

    Add Schema Snapshots & Breaking Changes CI Step (#15)
    * generate schema snapshot every server start
    
    * add cli argument to only generate snapshot
    
    * use snapshot instead of generated schema in /ui
    
    * add snapshot test
    
    * fix snapshot
    
    * rename snapshot file
    
    * add breaking changes check to CI
    
    * use correct command
    
    * fix snapshot path

--- packages/server/src/graphql/index.ts
+++ packages/server/src/graphql/index.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { buildSchema } from 'type-graphql'
 
 import { BoardgameResolver } from '@/modules/boardgame/boardgame.resolvers'
@@ -5,8 +6,11 @@ import { ClubResolver } from '@/modules/club/club.resolvers'
 import { MatchResolver } from '@/modules/match/match.resolvers'
 import { UserResolver } from '@/modules/user/user.resolvers'
 
-export const createSchema = async () =>
+export const createSchema = async (generateSnapshot = true) =>
   buildSchema({
+    emitSchemaFile: !generateSnapshot
+      ? false
+      : { path: resolve(__dirname, 'snapshot.graphql') },
     dateScalarMode: 'isoDate',
     resolvers: [BoardgameResolver, ClubResolver, MatchResolver, UserResolver],
   })
