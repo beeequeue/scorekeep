2454e27b1073515c5bddcf272e990bf97ba32489
Adam <adam.lj.haglund@gmail.com>
2019-12-23 01:18:47 +0100

    add colors

---
+++ packages/ui/src/design.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  background: {
+    body: '#101516',
+    primary: '#182225',
+    secondary: '#1e2b2e',
+  },
+  text: {
+    primary: '#eee',
+    secondary: '#a1bab3',
+    tertiary: '#6c9389',
+  },
+  highlights: {
+    gradients: {
+      main: 'linear-gradient(90deg, #1AD692, #5CDBE7)',
+    },
+  },
+} as const
