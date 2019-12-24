3083ce1750cc9125ff048c9c522f4869852a3947
Adam <adam.lj.haglund@gmail.com>
2019-12-24 21:44:56 +0100

    re-add missing rollup config

---
+++ packages/constants/rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from "rollup-plugin-typescript2"
+import babel from "rollup-plugin-babel"
+import { DEFAULT_EXTENSIONS } from "@babel/core"
+import pkg from "./package.json"
+
+const extensions = [...DEFAULT_EXTENSIONS, ".ts", ".tsx"]
+
+/**
+ * @type InputOptions
+ */
+export default {
+  input: "./src/index.ts",
+  plugins: [
+    typescript(),
+    babel({ include: "src/**/*", extensions, externalHelpers: true })
+  ],
+  external: [...Object.keys(pkg.peerDependencies), ...Object.keys(pkg.dependencies)],
+  preserveModules: true,
+  output: [
+    {
+      dir: "lib/es",
+      format: "es"
+    },
+    {
+      dir: "lib/cjs",
+      format: "cjs"
+    }
+  ]
+}
