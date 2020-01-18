7fec68e45ef1c65621ef88ec66fb1114e757e1b1
Adam <adam.lj.haglund@gmail.com>
2020-01-18 20:27:03 +0100

    move test data

--- packages/server/src/utils/test-data.ts
+++ packages/server/src/utils/test-data.ts
@@ -3,6 +3,39 @@ import faker from 'faker'
 
 faker.seed(12)
 
+const defaultSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema#' as const,
+  type: 'object' as const,
+  required: ['player', 'winner', 'final'],
+  properties: {
+    player: {
+      type: 'string' as const,
+    },
+    winner: {
+      type: 'boolean' as const,
+    },
+    final: {
+      type: 'number' as const,
+    },
+  },
+}
+
+const defaultResultGenerator = (users: Array<{ uuid: string }>) => {
+  const winner = faker.random.arrayElement(users)
+
+  return users.map(user => {
+    const isWinner = winner.uuid === user.uuid
+
+    return {
+      player: user.uuid,
+      winner: isWinner,
+      final: isWinner
+        ? faker.random.number({ min: 50, max: 80 })
+        : faker.random.number(50),
+    }
+  })
+}
+
 export const GAMES = {
   azul: {
     boardgame: new Boardgame({
@@ -16,38 +49,9 @@ export const GAMES = {
       rulebook: null,
       minPlayers: 2,
       maxPlayers: 4,
-      resultsSchema: {
-        $schema: 'http://json-schema.org/draft-07/schema#',
-        type: 'object',
-        required: ['player', 'winner', 'final'],
-        properties: {
-          player: {
-            type: 'string' as const,
-          },
-          winner: {
-            type: 'boolean' as const,
-          },
-          final: {
-            type: 'number' as const,
-          },
-        },
-      },
+      resultsSchema: defaultSchema,
     }),
-    generateResult: (users: Array<{ uuid: string }>) => {
-      const winner = faker.random.arrayElement(users)
-
-      return users.map(user => {
-        const isWinner = winner.uuid === user.uuid
-
-        return {
-          player: user.uuid,
-          winner: isWinner,
-          final: isWinner
-            ? faker.random.number({ min: 50, max: 80 })
-            : faker.random.number(50),
-        }
-      })
-    },
+    generateResult: defaultResultGenerator,
   },
   scythe: {
     boardgame: new Boardgame({
@@ -137,4 +141,81 @@ export const GAMES = {
       })
     },
   },
+  mars: {
+    boardgame: new Boardgame({
+      type: GAME_TYPE.COMPETITIVE,
+      name: 'Terraforming Mars (mock)',
+      shortName: 'terraforming-mars',
+      aliases: [
+        'A Mars terraformálása',
+        'Mars: Teraformace',
+        'Teraformarea Planetei Marte',
+        'Terraformacja Marsa',
+        'Покорение Марса',
+        'Тераформирай Марс',
+        'Тераформування Марса',
+        'พลิกพิภพดาวอังคาร',
+        'テラフォーミング・マーズ',
+        '殖民火星',
+        '테라포밍 마스',
+      ],
+      thumbnail:
+        'https://cf.geekdo-images.com/itemrep/img/bhemoxL7PG1a_79L0D9syPTADSY=/fit-in/246x300/pic3536616.jpg',
+      url: 'https://boardgamegeek.com/boardgame/167791/terraforming-mars',
+      rulebook: null,
+      minPlayers: 1,
+      maxPlayers: 5,
+      resultsSchema: defaultSchema,
+    }),
+    generateResult: defaultResultGenerator,
+  },
+  gloomhaven: {
+    boardgame: new Boardgame({
+      type: GAME_TYPE.COMPETITIVE,
+      name: 'Gloomhaven (mock)',
+      shortName: 'gloomhaven',
+      aliases: [
+        'Gloomhaven.Мрачная Гавань',
+        'Homályrév',
+        '幽港迷城',
+        '글룸헤이븐',
+      ],
+      thumbnail:
+        'https://cf.geekdo-images.com/itemrep/img/P7MVqNuhAl8Y4fxiM6e74kMX6e0=/fit-in/246x300/pic2437871.jpg',
+      url: 'https://boardgamegeek.com/boardgame/174430/gloomhaven',
+      rulebook: null,
+      minPlayers: 1,
+      maxPlayers: 4,
+      resultsSchema: defaultSchema,
+    }),
+    generateResult: defaultResultGenerator,
+  },
+  wingspan: {
+    boardgame: new Boardgame({
+      type: GAME_TYPE.COMPETITIVE,
+      name: 'Wingspan (mock)',
+      shortName: 'wingspan',
+      aliases: [
+        'Fesztáv',
+        'Flügelschlag',
+        'Na křídlech',
+        'Na skrzydłach',
+        'Крылья',
+        'ปีกปักษา',
+        '展翅翱翔',
+        '윙스팬',
+      ],
+      thumbnail:
+        'https://cf.geekdo-images.com/itemrep/img/vb971Kg92dzMd1TM3RBJtQm-XCU=/fit-in/246x300/pic4458123.jpg',
+      url: 'https://boardgamegeek.com/boardgame/266192/wingspan',
+      rulebook: null,
+      minPlayers: 1,
+      maxPlayers: 5,
+      resultsSchema: defaultSchema,
+    }),
+    generateResult: defaultResultGenerator,
+  },
 } as const
+
+export const getTestBoardgames = () =>
+  Object.keys(GAMES).map(key => GAMES[key as keyof typeof GAMES].boardgame)
